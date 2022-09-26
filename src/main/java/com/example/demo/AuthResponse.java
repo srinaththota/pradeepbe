b23f133eb6ceebae2335e626719fa6926674de4f
srinaththota <srinath.thota@gmail.com>
2022-09-26 18:20:51 +0200

    added

---
+++ src/main/java/com/example/demo/AuthResponse.java
@@ -0,0 +1,15 @@
+package models;
+
+public class AuthResponse {
+
+	private final String jwt;
+
+	public AuthResponse(String jwt) {
+		super();
+		this.jwt = jwt;
+	}
+
+	public String getJwt() {
+		return jwt;
+	}
+}
---
+++ src/main/java/com/example/demo/MyUserDetails.java
@@ -0,0 +1,71 @@
+package models;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class MyUserDetails implements UserDetails{
+
+	static final long serialVersionUID = 42L;
+	
+	private String username;
+	private String password;
+	private boolean active;
+	private List<GrantedAuthority> authorities;
+	
+	public MyUserDetails(UserDetails user) {
+		this.username=user.getUsername();
+		this.password=user.getPassword();
+		this.active=true;
+//		this.authorities= Arrays.stream(user.getAuthorities().split)
+		
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public List<GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+	public void setAuthorities(List<GrantedAuthority> authorities) {
+		this.authorities = authorities;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+}
